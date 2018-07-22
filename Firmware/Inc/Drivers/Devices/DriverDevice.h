58656fba4f325164cba4dd569a1865eaf063bf03
Unknown <utu.kos@gmail.com>
2018-07-22 14:41:53 +0300

    Basic 'Device' implementation

---
+++ Firmware/Inc/Drivers/Devices/DriverDevice.h
@@ -0,0 +1,31 @@
+#ifndef __DRIVER_DEVICE_H
+#define __DRIVER_DEVICE_H
+
+#include <string>
+#include "DriverInterface.h"
+
+namespace Driver {
+	enum DeviceError {
+		DeviceNoError = 0x00
+	};
+
+	template <typename T>
+	class Device {
+		private:
+			char m_description[16];
+			uint32_t m_id;
+			T m_interface;
+			DeviceError m_error;
+		
+		public:
+			Device(void) : m_error(DeviceNoError) { }
+			~Device(void) { }
+			
+			void setError(DeviceError error) { m_error = error; }
+			std::string getDescription(void) { return std::string(m_description); }
+			uint32_t getId(void) { return m_id; }
+			uint8_t getError(void) { return (uint8_t)m_error; }
+	};
+}
+
+#endif // __DRIVER_DEVICE_H
