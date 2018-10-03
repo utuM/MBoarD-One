377a7e7c12ba6e94d7463c67b71f65aa1a36fb71
Unknown <utu.kos@gmail.com>
2018-10-03 09:42:21 +0300

    Correcting 'Interface' class

--- Firmware/Inc/Drivers/Interfaces/DriverInterface.h
+++ Firmware/Inc/Drivers/Interfaces/DriverInterface.h
@@ -16,13 +16,16 @@ namespace Driver {
 			Interface(void) : m_error(InterfaceNoError) { }
 			~Interface(void) { }
 			
+            virtual void setHandler(void* handler) = 0;
 			void setError(const InterfaceError code) { m_error = code; }
 			uint8_t getError(void) { return (uint8_t)m_error; }
 			
-			virtual uint8_t sendByte(uint8_t byte) = 0;
-			virtual uint8_t sendData(uint8_t* data, uint16_t size) = 0;
-			virtual uint8_t recvByte(uint8_t* byte) = 0;
-			virtual uint8_t recvData(uint8_t* data, uint16_t size) = 0;
+			virtual uint8_t sendByte(uint8_t reg, uint8_t value) = 0;
+			virtual uint8_t sendData(uint8_t reg, uint8_t* data, 
+                                                             uint16_t size) = 0;
+			virtual uint8_t recvByte(uint8_t reg, uint8_t* value) = 0;
+			virtual uint8_t recvData(uint8_t reg, uint8_t* data, 
+                                                             uint16_t size) = 0;
 	};
 }
 
