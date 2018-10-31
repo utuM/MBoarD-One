2186280dd2bf1e4e5a21a6fc30739e3e6e1c22c0
Kostya <kostyantyn.komarov@droid-technologies.com>
2018-10-31 11:09:20 +0200

    Initialization fixing
    Added some registers writing to make the sensor works properly.
    Also checked data receiving process - success, but received raw data.

--- Firmware/Inc/Drivers/Devices/Sensors/HeartRate/Max30100.h
+++ Firmware/Inc/Drivers/Devices/Sensors/HeartRate/Max30100.h
@@ -1,19 +1,74 @@
 #ifndef __SENSORS_MAX30100_H
 #define __SENSORS_MAX30100_H
 
-#include "DriverDevice.h"
-#include "DriverI2c.h"
-#include <math.h>
+#include <stdint.h>
 
-namespace Device {
-	const uint8_t s_kMax30100Address = 0;
+namespace Driver {
+	const uint8_t s_kMax30100Address = 0x57 << 1;
 	
-	class Max30100 : public Device<I2c>	{
+    enum Max30100Register {
+        kInterruptStatus = 0x00,
+        kInterruptEnable,
+        kFifoWritePtr,
+        kOverFlowCounter,
+        kFifoReadPtr,
+        kFifoDataRegister,
+        kModeConfig,
+        kSpo2Config,
+        kLedConfig = 0x09,
+        kTempInteger = 0x16,
+        kTempFraction,
+        kRevisionId = 0xFE,
+        kPartId
+    };
+    
+    enum Max30100Spo2SampleRate {
+        k50Samples = 0x00,
+        k100Samples = 0x04,
+        k167Samples = 0x08,
+        k200Samples = 0x0C,
+        k400Samples = 0x10,
+        k600Samples = 0x14,
+        k800Samples = 0x18,
+        k1000Samples = 0x1C
+    };
+    
+    enum Max30100LedPulseWidth {
+        k200us13bits = 0x00,
+        k400us14bits,
+        k800us15bits,
+        k1600us16bits
+    };
+    
+    enum Max30100LedCurrent {
+        k0p0mA = 0x00,
+        k4p4mA,
+        k7p6mA,
+        k11p0mA,
+        k14p0mA,
+        k17p4mA,
+        k20p8mA,
+        k24p0mA,
+        k27p1mA,
+        k30p6mA,
+        k33p8mA,
+        k37p0mA,
+        k40p2mA,
+        k43p6mA,
+        k46p8mA,
+        k50p0mA
+    };
+    
+	class Max30100 {
 		private:
+            void _init(void);
 		
 		public:
 			Max30100(void);
 			~Max30100(void);
+            
+            bool checkId(uint8_t& revision, uint8_t& part);
+            bool getHeartRate(void);
 	};
 }
 
--- Firmware/Src/Drivers/Devices/Sensors/HeartRate/Max30100.cpp
+++ Firmware/Src/Drivers/Devices/Sensors/HeartRate/Max30100.cpp
@@ -1,11 +1,55 @@
 #include "Max30100.h"
+#include "i2c.h"
+
+#define     MAX30100_STANDARD_PART_ID   0x11
+
+extern I2C_HandleTypeDef hi2c1;
+
+void Driver::Max30100::_init(void)
+{
+    // Configure working mode: enable temperature and measure
+    // only heart rate.
+    uint8_t l_config = 0x0A;
+    HAL_I2C_Mem_Write(&hi2c1, s_kMax30100Address, kModeConfig, 
+                      I2C_MEMADD_SIZE_8BIT, &l_config, 1, 1000);
+    // Configure SpO2 measurement.
+    l_config = 0x40 | k50Samples | k1600us16bits;
+    HAL_I2C_Mem_Write(&hi2c1, s_kMax30100Address, kSpo2Config, 
+                      I2C_MEMADD_SIZE_8BIT, &l_config, 1, 1000);
+    // Configure Red and IR LEDs current levels.
+    l_config = (k11p0mA << 4) | k11p0mA;
+    HAL_I2C_Mem_Write(&hi2c1, s_kMax30100Address, kLedConfig, 
+                      I2C_MEMADD_SIZE_8BIT, &l_config, 1, 1000);
+}
 
 Driver::Max30100::Max30100(void)
 {
-	
+	_init();
 }
 
 Driver::Max30100::~Max30100(void)
 {
 	
+}
+
+bool Driver::Max30100::checkId(uint8_t& revision, uint8_t& part)
+{
+    // Read ID registers.
+    uint8_t l_buffer[2] = {0};
+    HAL_I2C_Mem_Read(&hi2c1, s_kMax30100Address, kRevisionId, 
+                     I2C_MEMADD_SIZE_8BIT, &l_buffer[0], 2, 1000);
+    revision = l_buffer[0];
+    part = l_buffer[1];
+    // Check 'Part ID' value is device valid.
+    return (part == MAX30100_STANDARD_PART_ID ? true : false);
+}
+
+bool Driver::Max30100::getHeartRate(void)
+{
+    uint8_t l_byte[8] = {0};
+    HAL_I2C_Mem_Read(&hi2c1, s_kMax30100Address, kFifoDataRegister, 
+                     I2C_MEMADD_SIZE_8BIT, &l_byte[0], 1, 1000);
+    printf("%u\n", l_byte[0]);
+
+    return true;
 }
