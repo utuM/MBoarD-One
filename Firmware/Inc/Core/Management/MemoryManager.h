4056c89b5fda63c1c6381ae0a792f3e49caa150c
Kostya <kostyantyn.komarov@droid-technologies.com>
2018-11-01 11:36:56 +0200

    Basic memory manager class implementation

---
+++ Firmware/Inc/Core/Management/MemoryManager.h
@@ -0,0 +1,15 @@
+#ifndef __CORE_MEMORY_MANAGER_H
+#define __CORE_MEMORY_MANAGER_H
+
+namespace Core {
+    class MemoryManager {
+        private:
+            
+        
+        public:
+            MemoryManager(void);
+            ~MemoryManager(void);
+    };
+}
+
+#endif // __CORE_MEMORY_MANAGER_H
