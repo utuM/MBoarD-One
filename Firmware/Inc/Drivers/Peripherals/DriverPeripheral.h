57eb3bc66491b416065e884b729e1a0e298cfc0e
Kostya <kostyantyn.komarov@droid-technologies.com>
2018-11-20 11:04:00 +0200

    Properly peripheral class implementation

--- Firmware/Inc/Drivers/Peripherals/DriverPeripheral.h
+++ Firmware/Inc/Drivers/Peripherals/DriverPeripheral.h
@@ -1,14 +1,33 @@
-#ifndef __WORKFLOW_H
-#define __WORKFLOW_H
+#ifndef __DRIVER_PERIPHERAL_H
+#define __DRIVER_PERIPHERAL_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+ *	@file		DriverPeripheral.h
+ *	@version	0.1
+ *	@author		utuM (Kostyantyn Komarov)
+ *	@date		20.11.2018 (implementation)
+ *  @brief		Peripheral controller module class implementation.
+ *				This class describes peripheral device of selected 
+ *				microcontroller. 
+ */
 
-void execute(void);
-
-#ifdef __cplusplus
+namespace Driver {
+	/**
+	 *	Peripheral class implementation.
+	 **/
+	class Peripheral {
+		private:
+			char m_name[16];	///< Name of peripheral device.
+		
+		public:
+			// Class constructors.
+			Peripheral(void);
+			Peripheral(std::string name)
+			{
+				name = (name.length() >= 16 ? name.substr(0, 15), name);
+				sprintf(m_name, "%s", name.c_str());
+			}
+	};
 }
-#endif
 
-#endif // __WORKFLOW_H
+#endif // __DRIVER_PERIPHERAL_H
