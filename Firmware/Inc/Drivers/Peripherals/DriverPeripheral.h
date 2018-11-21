9bbcd3add647f209fac3131ff72214e63d26262d
Kostya <kostyantyn.komarov@droid-technologies.com>
2018-11-21 11:36:35 +0200

    Class minor improving.

--- Firmware/Inc/Drivers/Peripherals/DriverPeripheral.h
+++ Firmware/Inc/Drivers/Peripherals/DriverPeripheral.h
@@ -1,15 +1,17 @@
-#ifndef __DRIVER_PERIPHERAL_H
-#define __DRIVER_PERIPHERAL_H
-
 /**
  *	@file		DriverPeripheral.h
- *	@version	0.1
+ *	@version	0.2
  *	@author		utuM (Kostyantyn Komarov)
  *	@date		20.11.2018 (implementation)
  *  @brief		Peripheral controller module class implementation.
  *				This class describes peripheral device of selected 
  *				microcontroller. 
  */
+
+#ifndef __DRIVER_PERIPHERAL_H
+#define __DRIVER_PERIPHERAL_H
+
+#include <iostream>
 
 namespace Driver {
 	/**
@@ -21,12 +23,19 @@ namespace Driver {
 		
 		public:
 			// Class constructors.
-			Peripheral(void);
+            Peripheral(void) { }
 			Peripheral(std::string name)
 			{
-				name = (name.length() >= 16 ? name.substr(0, 15), name);
+				setName(name);
+			}
+			// Class setter.
+			void setName(std::string name)
+			{
+				name = (name.length() >= 16 ? name.substr(0, 15) : name);
 				sprintf(m_name, "%s", name.c_str());
 			}
+			// Class getter.
+			std::string getName(void) { return std::string(m_name); }
 	};
 }
 
