65b529217dd14ed166686b4d3fad1082d988fd95
Kostyantyn <kostyantyn.komarov@droid-technologies.com>
2018-12-07 12:30:36 +0200

    Peripheral driver, release 1.0

--- Firmware/Include/Drivers/Peripherals/Peripheral.h
+++ Firmware/Include/Drivers/Peripherals/Peripheral.h
@@ -1,11 +1,11 @@
 /**
- *	@file		Peripheral.h
- *	@version	1.0
- *	@author		utuM (Kostyantyn Komarov)
- *	@date		20.11.2018 (release)
- *  @brief		Peripheral controller module class implementation.
- *				This class describes peripheral device of selected 
- *				microcontroller. 
+ *	@file       Peripheral.h (header file only)
+ *	@version    1.0
+ *	@author     utuM (Kostyantyn Komarov)
+ *	@date       7.12.2018 (release)
+ *  @brief      Peripheral controller module class implementation.
+ *              This class describes peripheral device of selected 
+ *              microcontroller. 
  */
 
 #ifndef __DRIVER_PERIPHERAL_H
@@ -14,46 +14,46 @@
 #include <iostream>
 
 namespace Driver {
-	/**
-	 *	Peripheral class implementation.
-	 **/
-	class Peripheral {
-		private:
-			char m_name[16];	///< Name of peripheral device.
+    /**
+     *	Peripheral class implementation.
+     **/
+    class Peripheral {
+        private:
+            char m_name[16];	///< Name of peripheral device.
 		
 		public:
-			// Class constructors and desctructor.
-            Peripheral(void) { }
-			/**
-			 * Peripheral class constructor.
-			 * @param[i] std::string name - name of current peripheral device.
-			 **/
-			Peripheral(const std::string name)
-			{
-				setName(name);
-			}
-			~Peripheral(void) { }
+            // Class constructors and desctructor.
+            Peripheral(void) { /* Leave empty for default one. */ }
+            /**
+             * Peripheral class constructor.
+             * @param[i] const std::string name - name of current peripheral
+                                                  device.
+             **/
+            Peripheral(const std::string name)
+            {
+                setName(name);
+            }
+            ~Peripheral(void) { }
 			
-			// Class setter and getter.
+            // Class setter and getter.
+            /**
+             * Set peripheral device name.
+             * @param[i] std::string name - new name to be setting up.
+             **/
+            void setName(std::string name)
+            {
+                name = (name.length() >= 16 ? name.substr(0, 15) : name);
+                sprintf(m_name, "%s", name.c_str());
+            }
 			
-			/**
-			 * Set peripheral device name.
-			 * @param[i] std::string name - new name to be setting up.
-			 **/
-			void setName(const std::string name)
-			{
-				name = (name.length() >= 16 ? name.substr(0, 15) : name);
-				sprintf(m_name, "%s", name.c_str());
-			}
-			
-			/**
-			 * Get peripheral device name.
-			 **/
-			std::string getName(void)
-			{
-				return std::string(m_name);
-			}
-	};
+            /**
+             * Get peripheral device name.
+             **/
+            std::string getName(void)
+            {
+                return std::string(m_name);
+            }
+    };
 }
 
 #endif // __DRIVER_PERIPHERAL_H
