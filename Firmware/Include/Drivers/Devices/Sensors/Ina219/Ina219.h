64725d99a1095e8a53c27ac60f59e0b803e60284
Kostyantyn <kostyantyn.komarov@droid-technologies.com>
2018-12-12 20:01:40 +0200

    Further implementation and body structure

--- Firmware/Include/Drivers/Devices/Sensors/Ina219/Ina219.h
+++ Firmware/Include/Drivers/Devices/Sensors/Ina219/Ina219.h
@@ -15,41 +15,117 @@
 #include "I2c.h"
 
 namespace Driver {
-    const uint8_t s_kIna219Address = 0xEC; ///< Default INA219 address on 
+    const uint8_t s_kIna219Address = 0x40; ///< Default INA219 address on 
                                            ///  I2C bus.
 
     /**
      * INA219 sensor registers.
      **/
     enum Ina219Registers : uint8_t {
-
+        kInaConfiguration      = 0x00, ///< Sensor configuration.
+        kInaShuntVoltage       = 0x01, ///< Shunt voltage value.
+        kInaBusVoltage         = 0x02, ///< Bus voltage value.
+        kInaPower              = 0x03, ///< Power value.
+        kInaCurrent            = 0x04, ///< Current value.
+        kInaCalibration        = 0x05  ///< Calibration value.
     };
+
+    /**
+     * INA219 PGA settings (use for sending to 'Configuration' register).
+     **/
+    enum Ina219ConfigureValues : uint16_t {
+        kInaReset              = 0x8000, ///< Reset configuration.
+        kInaBusVRange          = 0x2000, ///< Bus voltage range is 32V.
+        kInaGain1              = 0x0000, ///< Voltage range is +- 40mV.
+        kInaGain2              = 0x0800, ///< Voltage range is +- 80mV.
+        kInaGain4              = 0x1000, ///< Voltage range is +- 160mV.
+        kInaGain8              = 0x1800  ///< Voltage range is +- 320mV.
+	};
 	
+    /**
+     * INA219 bus ADC resolution (use for sending to 'Configuration' register).
+     **/
+    enum Ina219BusAdcResolution : uint16_t {
+        kInaBus9bit            = 0x0000, ///< 9-bit ADC resolution.
+        kInaBus10bit           = 0x0080, ///< 10-bit ADC resolution.
+        kInaBus11bit           = 0x0100, ///< 11-bit ADC resolution.
+        kInaBus12bit           = 0x0180, ///< 12-bit ADC resolution.
+        kInaBus2Samples        = 0x0480, ///< 2 samples.
+        kInaBus4Samples        = 0x0500, ///< 4 samples.
+        kInaBus8Samples        = 0x0580, ///< 8 samples.
+        kInaBus16Samples       = 0x0600, ///< 16 samples.
+        kInaBus32Samples       = 0x0680, ///< 32 samples.
+        kInaBus64Samples       = 0x0700, ///< 64 samples.
+        kInaBus128Samples      = 0x0780  ///< 128 samples.
+	};
+	
+    /**
+     * INA219 shunt ADC resolution (use for sending to 'Configuration' register).
+     **/
+    enum Ina219ShuntAdcResolution : uint16_t {
+        kInaShunt9bit          = 0x0000, ///< 9-bit ADC resolution.
+        kInaShunt10bit         = 0x0008, ///< 10-bit ADC resolution.
+        kInaShunt11bit         = 0x0010, ///< 11-bit ADC resolution.
+        kInaShunt12bit         = 0x0018, ///< 12-bit ADC resolution.
+        kInaShunt2Samples      = 0x0048, ///< 2 samples.
+        kInaShunt4Samples      = 0x0050, ///< 4 samples.
+        kInaShunt8Samples      = 0x0058, ///< 8 samples.
+        kInaShunt16Samples     = 0x0060, ///< 16 samples.
+        kInaShunt32Samples     = 0x0068, ///< 32 samples.
+        kInaShunt64Samples     = 0x0070, ///< 64 samples.
+        kInaShunt128Samples    = 0x0078  ///< 128 samples.
+	};
+
+    /**
+     * INA219 measurement modes (use for sending to 'Configuration' register).
+     **/
+    enum Ina219ModeSettings : uint16_t {
+        kInaPowerDown          = 0x0000, ///< Power-down mode.
+        kInaShuntVTriggered    = 0x0001, ///< Shunt voltage, triggered.
+        kInaBusVTriggered      = 0x0002, ///< Bus voltage, triggered.
+        kInaShuntBusVTriggered = 0x0003, ///< Shunt/bus voltage, triggered.
+        kInaAdcDisabled        = 0x0004, ///< ADC off (disabled).
+        kInaShuntVContinued    = 0x0005, ///< Shunt voltage, continuous.
+        kInaBusVContinued      = 0x0006, ///< Bus voltage, continuous.
+        kInaShuntBusVContinued = 0x0007  ///< Shunt/bus voltage, continuous.
+    };
+
     /**
      * INA219 possible errors.
      **/
-    enum Ina219ErrorCodes {
-        kInaNoError            = 0x00  ///< No error.
+    enum Ina219ErrorCodes : uint8_t {
+        kInaNoError = 0x00,           ///< No error.
+        kInaErrInterfaceIsntInit,     ///< Some error was occured during
+                                      ///  interface initialization process.
+        kInaErrIsntInit,              ///< The sensor wasn't properly
+                                      ///  initialized.
+        kInaErrReset,                 ///< Cannot send reset bit to the sensor.
+        kInaErrSettingUp,             ///< Cannot send settings value to the
+                                      ///  sensor.
+        kInaErrReceiveRegisterValue,  ///< Cannot receive a value from the
+                                      ///  requested register.
+        kInaErrSendRegisterValue      ///< Cannot send a value into the 
+                                      ///  requested register.
     };
     
     class Ina219 : public Device<I2c> {
         private:
-            bool m_isInit;                        ///< Successful initialization
-                                                  ///  flag.
-
-            Ina219ErrorCodes m_error;             ///< Current error.
+            bool m_isInit;            ///< Successful initialization flag.
+            float m_voltage;          ///< Voltage value.
+            float m_current;          ///< Current value.
+            Ina219ErrorCodes m_error; ///< Current error.
             
-            Ina219ErrorCodes _init(Bme280Patterns pattern);
+            Ina219ErrorCodes _init(void);
             
         public:
             // Contructors and desctructor.
-            Ina219(void);
-            // Ina219( );
+            Ina219(void) : m_isInit(false) { /* To prevent false init. */ }
+            Ina219(Ina219ModeSettings mode);
             ~Ina219(void);
-            
             // Getter.
             uint8_t getError(void) { return (uint8_t)m_error; }
-            
+            // Basic methods.
+            Ina219ErrorCodes update(float& rVoltage, float& rCurrent);
             Ina219ErrorCodes toggle(bool isPowerOn);
     };
 }
--- Firmware/Source/Drivers/Devices/Sensors/Ina219/Ina219.cpp
+++ Firmware/Source/Drivers/Devices/Sensors/Ina219/Ina219.cpp
@@ -9,3 +9,125 @@
 
 #include "Ina219.h"
 
+// Place 16-bit value into 2-celled buffer.
+#define PUSH_16BIT(x, y) {     \
+    y[0] = (uint8_t)(x >> 8);  \
+    y[1] = x;                  \
+}                              \
+
+// Take 2-celled buffer into 16-bit value.
+#define PULL_16BIT(x, y) {     \
+    *x = (uint16_t)y[0] << 8 | \
+                   y[1];       \
+}                              \
+
+/**
+ * INA219 sensor initialization.
+ * @return kInaNoError            - successful initialization;
+ *         kInaErrCannotReset     - error on sending reset bit;
+ *         kInaErrCannotSettingUp - error on sending settings.
+ **/
+Driver::Ina219ErrorCodes Driver::Ina219::_init(void)
+{
+    uint16_t l_settings = 0;
+    uint8_t l_buffer[2] = { 0 };
+	
+    // Power on device.
+    toggle(true);
+    // Reset the sensor.
+    PUSH_16BIT((uint16_t)kInaReset, l_buffer);
+    if (m_interface.sendData(kInaConfiguration, l_buffer, 2)) {
+        return (m_error = kInaErrReset);	
+    }
+    // Setting up device.
+    l_settings = kInaGain1 | kInaBus12bit | kInaBusVContinued;
+    PUSH_16BIT(l_settings, l_buffer);
+    if (m_interface.sendData(kInaConfiguration, l_buffer, 2)) {
+        return (m_error = kInaErrSettingUp);	
+    }
+    
+	m_isInit = true;
+    return (m_error = kInaNoError);
+}
+
+/**
+ * INA219 class constructor.
+ * @param[i] Ina219ModeSettings mode - requested sensor work mode.
+ **/
+Driver::Ina219::Ina219(Ina219ModeSettings mode) : m_isInit(false),
+                                                  m_voltage(0.0f),
+                                                  m_current(0.0f),
+                                                  m_error(kInaNoError)
+{
+    // Init the sensor work mode if current interface is worked.
+    I2cParameters l_parameters = {
+        0,
+        s_kIna219Address,
+        kI2cNormalMode,
+        true
+    };
+    // Setting up the sensor only if interface is initialized correctly.
+    if (!m_interface.init(l_parameters)) {
+        _init();
+    }
+    else {
+        m_error = kInaErrInterfaceIsntInit;
+    }
+}
+
+/**
+ * INA219 class desctructor.
+ **/
+Driver::Ina219::~Ina219(void)
+{
+    // Turn the sensor into the sleep mode on object destruction.
+    if (m_isInit && m_error == kInaNoError) {
+        toggle(false);
+        m_isInit = false;
+    }
+}
+
+/**
+ * INA219 update sensor.
+ **/
+Driver::Ina219ErrorCodes Driver::Ina219::update(float& rVoltage, float& rCurrent)
+{
+
+}
+
+/**
+ * INA219 sensor power on/off.
+ * @param[i] bool isTurnedOn - flag that decides to turn on/off the sensor.
+ * @return kInaNoError          - successful mode toggling;
+ *         kInaErrIsntInit      - the sensor wasn't initialized properly;
+ *         kInaErrSendingOpcode - cannot send opcode via used interface.
+ **/
+Driver::Ina219ErrorCodes Driver::Ina219::toggle(bool isTurnedOn)
+{
+    // Check if the sensor was properly initialized.
+    if (!m_isInit) {
+        return (m_error = kInaErrIsntInit);
+    }
+  
+    uint16_t l_value = 0;
+    uint8_t l_buffer[2] = { 0 };
+	// Read current value from the configuration register.
+	if (m_interface.receiveData(kInaConfiguration, l_buffer, 2)) {
+        return (m_error = kInaErrReceiveRegisterValue);
+	}
+    PULL_16BIT(&l_value, l_buffer);
+    // Turn sensor into requested mode.
+    if (isTurnedOn) {
+        l_value |= kInaBusVContinued;
+    }
+    else {
+        l_value &= 0xFFF8;
+    }
+    // Send updated configuration value.
+    PUSH_16BIT(l_value, l_buffer);
+	if (m_interface.sendData(kInaConfiguration, l_buffer, 2)) {
+        return (m_error = kInaErrSendRegisterValue);
+	}
+    
+    return (m_error = kInaNoError);
+}
