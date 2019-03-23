94838960403203d5fedd749c818f9507dbfef6e7
Kostyantyn <kostyantyn.komarov@droid-technologies.com>
2019-03-23 18:31:53 +0200

    Driver release version

--- Firmware/Include/Drivers/Peripherals/Gpio/Gpio.h
+++ Firmware/Include/Drivers/Peripherals/Gpio/Gpio.h
@@ -1,9 +1,17 @@
 /**
   * @file    Gpio.h
-  * @version 0.1.0a
+  * @version 1.0.0
   * @author  utuM (Kostyantyn Komarov)
   * @data    19.03.2019 (creation)
-  * @brief   -//-
+  * @data    23.03.2019 (release)
+  * @brief   GPIO driver class implementation.
+  *          Current driver can initializes every input and output pins are
+  *          required. Every pin is represented as a 'InputPin' or 'OutputPin'
+  *          class object. Every time on pin initialization, binded port will
+  *          be enabling using RCC staff.
+  *          For input pins, there is only 'getPinState()'. For output pins,
+  *          there are 'setPinState()' and 'togglePin()'.
+  *          On object desctructure, current pin will be de-initialized.
   **/
 
 #ifndef __PERIPHERAL_GPIO_H
@@ -11,37 +19,12 @@
 
 #include "Peripheral.h"
 #include "stm32h7xx_hal.h"
+#include "stm32h7xx_hal_gpio.h"
 
-#define GPIO_TYPE_INPUT    0x01
-#define GPIO_TYPE_OUTPUT   0x02
-
-namespace Driver {
-	class Gpio : public Peripheral {
-		private:	
-			enum GpioError {
-				kGpioNoError = 0x00
-			};
-		
-			bool m_isInit;
-			GpioError m_error;
-			
-			// Private class methods.
-			GpioError _init();
-			GpioError _deinit();
-			
-		public:			
-			enum GpioPort {
-                kGpioPortA = GPIOA,
-                kGpioPortB = GPIOB,
-                kGpioPortC = GPIOC,
-                kGpioPortD = GPIOD,
-                kGpioPortE = GPIOE,
-                kGpioPortF = GPIOF,
-                kGpioPortG = GPIOG,
-                kGpioPortH = GPIOH
-			};
-			
-			enum GpioPin {
+namespace Driver {    
+    class Gpio : public Peripheral {
+        public :
+            enum GpioPin : uint16_t {
                 kGpioPin0  = GPIO_PIN_0,
                 kGpioPin1  = GPIO_PIN_1,
                 kGpioPin2  = GPIO_PIN_2,
@@ -58,14 +41,91 @@ namespace Driver {
                 kGpioPin13 = GPIO_PIN_13,
                 kGpioPin14 = GPIO_PIN_14,
                 kGpioPin15 = GPIO_PIN_15
-			};
-		
-			Gpio(void) : m_isInit(false) { /* To prevent false init. */ }
-			Gpio(GpioPort port, GpioPin pin);
-			~Gpio(void);
-			// Getters and setters.
-			uint8_t getError(void) { return (uint8_t)m_error; }
-	};
+            };
+      
+        private :            
+            std::string m_name;
+            GPIO_TypeDef* m_port;
+            GpioPin m_pin;
+          
+        public :          
+            enum GpioPort : uint32_t {
+#if defined(STM32H743xx)
+                kGpioPortA = (uint32_t)GPIOA,
+                kGpioPortB = (uint32_t)GPIOB,
+                kGpioPortC = (uint32_t)GPIOC,
+                kGpioPortD = (uint32_t)GPIOD,
+                kGpioPortE = (uint32_t)GPIOE,
+                kGpioPortF = (uint32_t)GPIOF,
+                kGpioPortG = (uint32_t)GPIOG,
+                kGpioPortH = (uint32_t)GPIOH
+#elif
+                error("ERROR! List of ports is not defined!");
+#endif
+            };
+            
+            enum GpioSpeed : uint32_t {
+                kGpioSpeedLow      = GPIO_SPEED_FREQ_LOW,
+                kGpioSpeedMedium   = GPIO_SPEED_FREQ_MEDIUM,
+                kGpioSpeedHigh     = GPIO_SPEED_FREQ_HIGH,
+                kGpioSpeedVeryHigh = GPIO_SPEED_FREQ_VERY_HIGH
+            };
+            
+            Gpio(void)  { /* Leave empty for prevent false init. */ }
+            Gpio(std::string name) { m_name = name; }
+            ~Gpio(void) { /* Leave empty for prevent false init. */ } 
+            
+            void setPort(GpioPort port) { m_port = (GPIO_TypeDef*)port; }
+            void setPin(GpioPin pin) { m_pin = pin; }
+            std::string getPinName(void) { return m_name; }
+            GPIO_TypeDef* getPort(void) { return m_port; }
+            GpioPin getPin(void) { return m_pin; }
+            
+            void enablePort(GpioPort port);
+            void disablePort(GpioPort port);
+    };
+    
+    class InputPin : public Gpio {
+        private:
+            /* Let it be empty. */
+          
+        public:
+            enum Pull : uint32_t {
+                kInputNoPull   = GPIO_NOPULL,
+                kInputPullUp   = GPIO_PULLUP,
+                kInputPullDown = GPIO_PULLDOWN,
+            };
+          
+            InputPin(std::string name,
+                     GpioPort port,
+                     GpioPin pin,
+                     GpioSpeed speed,
+                     Pull pull);
+            ~InputPin(void);
+            
+            bool getPinState(void);
+    };
+    
+    class OutputPin : public Gpio {
+        private:
+            /* Let it be empty. */
+          
+        public:
+            enum State : uint8_t {
+                kStateReset = 0x00,
+                kStateSet
+            };
+          
+            OutputPin(std::string name,
+                      GpioPort port,
+                      GpioPin pin,
+                      GpioSpeed speed,
+                      State state);
+            ~OutputPin(void);
+            
+            void setPinState(State state);
+            void togglePin(void);
+    };
 }
 
 #endif // __PERIPHERAL_GPIO_H
--- Firmware/Source/Drivers/Peripherals/Gpio/Gpio.cpp
+++ Firmware/Source/Drivers/Peripherals/Gpio/Gpio.cpp
@@ -1,29 +1,231 @@
 /**
   * @file    Gpio.cpp
-  * @version 0.1.0a
+  * @version 1.0.0
   * @author  Kostyantyn Komarov (utuM)
   * @data    19.03.2019 (creation)
-  * @brief   -//-
+  * @data    23.03.2019 (release)
+  * @brief   GPIO driver class implementation.
+  *          Current driver can initializes every input and output pins are
+  *          required. Every pin is represented as a 'InputPin' or 'OutputPin'
+  *          class object. Every time on pin initialization, binded port will
+  *          be enabling using RCC staff.
+  *          For input pins, there is only 'getPinState()'. For output pins,
+  *          there are 'setPinState()' and 'togglePin()'.
+  *          On object desctructure, current pin will be de-initialized.
   **/
 
 #include "Gpioj.h"
 
-Driver::GpioError Driver::Gpio::_init()
+/**
+  * Enable port by input port index.
+  * @param[i] port - index of port to be activated.
+  * @return None.
+  **/
+void Driver::Gpio::enablePort(Gpio::GpioPort port)
 {
-	
+    // Check which port need to be activated.
+    switch (port) {
+        case Gpio::kGpioPortA : {
+            __HAL_RCC_GPIOA_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortB : {
+            __HAL_RCC_GPIOB_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortC : {
+            __HAL_RCC_GPIOC_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortD : {
+            __HAL_RCC_GPIOD_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortE : {
+            __HAL_RCC_GPIOE_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortF : {
+            __HAL_RCC_GPIOF_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortG : {
+            __HAL_RCC_GPIOG_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortH : {
+            __HAL_RCC_GPIOH_CLK_ENABLE();
+            break;
+        }
+        
+        default : { 
+            break;
+        }
+    }
 }
 
-Driver::GpioError Driver::Gpio::_deinit()
+/**
+  * Disable port by input port index.
+  * @param[i] port - index of port to be activated.
+  * @return None.
+  **/
+void Driver::Gpio::disablePort(Gpio::GpioPort port)
 {
-	
+    switch (port) {
+        case Gpio::kGpioPortA : {
+            __HAL_RCC_GPIOA_CLK_DISABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortB : {
+            __HAL_RCC_GPIOB_CLK_DISABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortC : {
+            __HAL_RCC_GPIOC_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortD : {
+            __HAL_RCC_GPIOD_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortE : {
+            __HAL_RCC_GPIOE_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortF : {
+            __HAL_RCC_GPIOF_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortG : {
+            __HAL_RCC_GPIOG_CLK_ENABLE();
+            break;
+        }
+        
+        case Gpio::kGpioPortH : {
+            __HAL_RCC_GPIOH_CLK_ENABLE();
+            break;
+        }
+        
+        default : { 
+            break;
+        }
+    }
+}
+    
+/**
+  * Input pin constructor with input parameters.
+  * @param[i] name  - pin description (can be empty).
+  * @param[i] port  - hardware port index that pin is occupied.
+  * @param[i] pin   - hardware pin index that pin is occupied.
+  * @param[i] speed - pin switching speed.
+  * @param[i] pull  - no-pull/pull activation. 
+  * @return None.
+  **/
+Driver::InputPin::InputPin(std::string name,
+                           Gpio::GpioPort port,
+                           Gpio::GpioPin pin,
+                           Gpio::GpioSpeed speed,
+                           Pull pull) : Gpio(name)
+{
+    // Enable port generating.
+    enablePort(port);
+    // Initialize input pin.
+    GPIO_InitTypeDef l_gpiSettings = {0};
+    l_gpiSettings.Pin = pin;
+    l_gpiSettings.Speed = speed;
+    l_gpiSettings.Pull = pull;
+    HAL_GPIO_Init((GPIO_TypeDef *)port, &l_gpiSettings);
+    // Save input port and pin.
+    setPort(port);
+    setPin(pin);
 }
 
-Driver::Gpio::Gpio()
+/**
+  * Input pin destructor.
+  * @return None.
+  **/
+Driver::InputPin::~InputPin(void)
 {
-	
+    HAL_GPIO_DeInit(getPort(), getPin());
 }
 
-Driver::Gpio::~Gpio(void)
+/**
+  * Get input pin state.
+  * @return true  - pin is 'High' level;
+  *         false - pin is 'Low' level.
+  **/
+bool Driver::InputPin::getPinState(void)
 {
-	
+    return HAL_GPIO_ReadPin(getPort(), getPin());
+}
+
+/**
+  * Output pin constructor with input parameters.
+  * @param[i] name  - pin description (can be empty).
+  * @param[i] port  - hardware port index that pin is occupied.
+  * @param[i] pin   - hardware pin index that pin is occupied.
+  * @param[i] speed - pin switching speed.
+  * @param[i] state - default pin level that need to be generated. 
+  * @return None.
+  **/
+Driver::OutputPin::OutputPin(std::string name,
+                             Gpio::GpioPort port,
+                             Gpio::GpioPin pin,
+                             Gpio::GpioSpeed speed,
+                             State state) : Gpio(name)
+{
+    // Enable port generating.
+    enablePort(port);
+    // Save input port and pin.
+    setPort(port);
+    setPin(pin);
+    // Initialize output pin.
+    setPinState(kStateReset);
+    GPIO_InitTypeDef l_gpiSettings = {0};
+    l_gpiSettings.Pin = pin;
+    l_gpiSettings.Speed = speed;
+    l_gpiSettings.Pull = Driver::InputPin::kInputNoPull;
+    l_gpiSettings.Mode = GPIO_MODE_OUTPUT_PP;
+    HAL_GPIO_Init((GPIO_TypeDef *)port, &l_gpiSettings);
+}
+
+/**
+  * Output pin destructor.
+  * @return None.
+  **/
+Driver::OutputPin::~OutputPin(void)
+{
+    HAL_GPIO_DeInit(getPort(), getPin());
+}
+
+/**
+  * Output pin level changing.
+  * @param[i] state - new level to be generated on current pin.
+  * @return None.
+  **/
+void Driver::OutputPin::setPinState(Driver::OutputPin::State state)
+{
+    HAL_GPIO_WritePin(getPort(), getPin(), (GPIO_PinState)state);
+}
+
+/**
+  * Output pin level toggling.
+  * @return None.
+  **/
+void Driver::OutputPin::togglePin(void)
+{
+    HAL_GPIO_TogglePin(getPort(), getPin());
 }
