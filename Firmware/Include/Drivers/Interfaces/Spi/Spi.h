f3c4456c4025e19e966e713e427c0734caa09c2f
Kostyantyn <kostyantyn.komarov@droid-technologies.com>
2019-04-22 10:23:49 +0300

    Valid implementation, non-done

--- Firmware/Include/Drivers/Interfaces/Spi/Spi.h
+++ Firmware/Include/Drivers/Interfaces/Spi/Spi.h
@@ -1,33 +1,96 @@
+/**
+ *  @file       Spi.h
+ *  @version    0.1 (header file)
+ *  @author     utuM (Kostyantyn Komarov)
+ *  @date       19.04.2019 (creation)
+ *  @brief      SPI interface class implementation.
+ **/
+
 #ifndef __INTERFACE_SPI_H
 #define __INTERFACE_SPI_H
 
 #include "Interface.h"
-#include "spi.h"
+#include "stm32f7xx_hal.h"
+#include "Typedefs.h"
 
-namespace Driver {
-    struct SpiParameters {
-		
-	};
-	
-	class Spi : public Interface {
-		private:
-		    bool m_isInit;                     ///< SPI initialization init.
-		
-		    void _deinit();
-		
-		public:
-		    SPI_HandleTypeDef* m_pHandler;
-		    // Constructors and destructor.
-			Spi(void) : m_isInit(false) { /* Leave empty. */ }
-			~Spi(void);
+namespace Driver
+{  
+    const u16 m_kSpiDefaultTimeout = 1000; ///< Default timeout value after
+                                           ///  which the data exchange process
+                                           ///  must be finished.
+  
+    /**
+      * SPI instance.
+      **/
+    enum SpiInstance : u32
+    {
+#if defined(STM32F746xx)
+        kSpi1 = SPI1_BASE,
+        kSpi2 = SPI2_BASE,
+        kSpi3 = SPI3_BASE,
+        kSpi4 = SPI4_BASE,
+        kSpi5 = SPI5_BASE,
+        kSpi6 = SPI6_BASE
+#else
+    #error "No SPI instance is defined!"
+#endif
+    };
+    
+    /**
+      * SPI interface baudrate prescalers.
+      **/
+    enum SpiSpeed : u32
+    {
+        kSpiPrescaler2   = 0x00000000,
+        kSpiPrescaler4   = 0x00000008,
+        kSpiPrescaler8   = 0x00000010,
+        kSpiPrescaler16  = 0x00000018,
+        kSpiPrescaler32  = 0x00000020,
+        kSpiPrescaler64  = 0x00000028,
+        kSpiPrescaler128 = 0x00000030,
+        kSpiPrescaler256 = 0x00000038
+    };
+  
+    /**
+      * Global SPI parameters for SPI class objects.
+      **/
+    struct SpiParameters
+    {
+        u8 m_index;       ///< Index of SPI bus to be in use.
+        SpiSpeed m_speed; ///< Interface prescaler.
+        
+        flag m_useDma;    ///< DMA usage flag (reserved for future).
+    };  
+    
+    /**
+      * SPI template class.
+      **/
+    class Spi : public Interface<SPI_HandleTypeDef, SpiParameters>
+    {
+        public:
+            SPI_HandleTypeDef m_handler;
+            
+            // Constructor and destructor.
+            Spi(void) : _m_isInit(no), _m_isDmaOn(no)
+            {
+                /* Leave empty. */
+            }
+            ~Spi(void);
+            
+            // Basic methods.
+            flag init(SpiParameters& rParameters);
+            flag sendByte(uint8_t reg, uint8_t value);
+            flag sendData(uint8_t reg, uint8_t* pData, const uint16_t size);
+            flag receiveByte(uint8_t reg, uint8_t& rValue);
+            flag receiveData(uint8_t reg, uint8_t* pData, const uint16_t size);
+      
+        private:
+            flag _m_isInit;  ///< Successful initialization flag.
+            flag _m_isDmaOn; ///< DMA usage flag (reserved for future usage).
 
-			// Basic methods.
-            bool init(SpiParameters& rParameters);
-            bool sendByte(uint8_t reg, uint8_t value);
-            bool sendData(uint8_t reg, uint8_t* pData, const uint16_t size);
-            bool receiveByte(uint8_t reg, uint8_t& rValue);
-            bool receiveData(uint8_t reg, uint8_t* pData, const uint16_t size);
-	};
+            // Private basic methods.
+            void _deinit(void);
+    };
 }
 
 #endif // __INTERFACE_SPI_H
--- Firmware/Source/Drivers/Interfaces/Spi/Spi.cpp
+++ Firmware/Source/Drivers/Interfaces/Spi/Spi.cpp
@@ -1,51 +1,227 @@
-#include "Spi.h"
+/**
+ *  @file       Spi.cpp
+ *  @version    0.1 (header file)
+ *  @author     utuM (Kostyantyn Komarov)
+ *  @date       19.04.2019 (creation)
+ *  @brief      SPI interface class implementation.
+ **/
 
-extern SPI_HandleTypeDef hspi1;
+#include "Spi_c.h"
 
-Driver::Spi::Spi(void)
+#include <string.h>
+
+#if defined(STM32F746xx)
+    #define MAXIMAL_SPI_HANDLERS_AMOUNT 6
+#else
+    #error "SPI handlers amount is 0!"
+#endif
+
+// SPI bus initialization flags.
+static bool s_isSpiInit[MAXIMAL_SPI_HANDLERS_AMOUNT] = 
 {
-    m_handler = &hspi1;
+    no, no, no, no, no, no
+};             
+                                
+// SPI global handlers.
+static SPI_HandleTypeDef s_globalSpiHandler[6] =
+{
+    0, 0, 0, 0, 0, 0
+};                                                            
+
+///**
+// * SPI MSP initialization.
+// * @param[i] pHandler - pointer to the SPI structure object.
+// **/
+//void HAL_SPI_MspInit(SPI_HandleTypeDef* pHandler)
+//{
+//    
+//}
+//
+///**
+// * SPI MSP deinitialization.
+// * @param[i] pHandler - pointer to the SPI structure object.
+// **/
+//void HAL_SPI_MspDeInit(SPI_HandleTypeDef* pHandler)
+//{
+//    
+//} 
+
+/**
+ * SPI initialization. 
+ * NOTICE!!! It must to be called to release transmission.
+ * @param[i] I2cParameters& rParams - reference to parameters structure object.
+ * return true - some error during initialization process;
+ *        false - no error.
+ **/
+bool Driver::Spi::init(SpiParameters& rParams)
+{  
+	// Check if requered SPI bus by index is not initialized.
+	if (!s_isSpiInit[rParams.m_index]) {
+		// Basic SPI handler initialization.
+		memset(&m_handler, 0x00, sizeof(SPI_HandleTypeDef));	
+		switch (rParams.m_index) {
+			case 0:
+				m_handler.Instance = (SPI_TypeDef*)kSpi1;
+				break;
+				
+			case 1:
+				m_handler.Instance = (SPI_TypeDef*)kSpi2;
+				break;
+                
+            case 2:
+                m_handler.Instance = (SPI_TypeDef*)kSpi3;
+                break;
+                
+            case 3:
+                m_handler.Instance = (SPI_TypeDef*)kSpi4;
+                break;
+              
+            case 4:
+                m_handler.Instance = (SPI_TypeDef*)kSpi5;
+                break;
+              
+            case 5:
+				m_handler.Instance = (SPI_TypeDef*)kSpi6;
+                break;
+              
+			default:
+				break;
+		}
+        // Setting up other constant parameters.
+        m_handler.Init.Mode = SPI_MODE_MASTER;
+        m_handler.Init.Direction = SPI_DIRECTION_2LINES;
+        m_handler.Init.DataSize = SPI_DATASIZE_8BIT;
+        m_handler.Init.CLKPolarity = SPI_POLARITY_LOW;
+        m_handler.Init.CLKPhase = SPI_PHASE_1EDGE;
+        m_handler.Init.NSS = SPI_NSS_SOFT;
+        m_handler.Init.BaudRatePrescaler = rParams.m_speed;
+        m_handler.Init.FirstBit = SPI_FIRSTBIT_MSB;
+        m_handler.Init.TIMode = SPI_TIMODE_DISABLE;
+        m_handler.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+        m_handler.Init.CRCPolynomial = 7;
+        m_handler.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+        m_handler.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+		if (HAL_SPI_Init(&m_handler) != HAL_OK) {
+			return true;
+		}
+		// Copying object's initialized handler into global one by bus index.
+		memcpy(&s_globalSpiHandler[rParams.m_index], &m_handler,
+                                                     sizeof(SPI_HandleTypeDef));     
+		s_isSpiInit[rParams.m_index] = true;
+	}
+	// Take the global copy of I2C instance and copy it if the bus is already.
+	else {
+		memcpy(&m_handler, &s_globalSpiHandler[rParams.m_index],
+                                                     sizeof(SPI_HandleTypeDef));
+	}
+
+    _m_isInit = true;
+    
+    return false;
 }
 
+/**
+ * SPI deinitialization. In real, this method make no deinitialization of the
+ * bus.
+ **/
+void Driver::Spi::_deinit(void)
+{
+    _m_isInit = false;
+}
+
+/**
+ * SPI class destructor.
+ **/
 Driver::Spi::~Spi(void)
 {
-    m_handler = nullptr;
+    _deinit();
 }
 
-/// TODO: change reloaded functions to handling SPI properly for every device.
-
-uint8_t Driver::Spi::sendByte(uint8_t reg, uint8_t value)
+/**
+ * I2C byte sending.
+ * @param[i] uint8_t reg - number of device's register.
+ * @param[i] uint8_t value - value that need to be sent.
+ * return true - some error was occured during transmission;
+ *        false - no error.
+ **/
+bool Driver::Spi::sendByte(uint8_t reg, uint8_t value)
 {
-    if (HAL_SPI_Transmit(m_handler, &value, 1, 0x1000) != HAL_OK) {
-        return 0x01;
+    if (!_m_isInit) {
+        return true;
+    }
+    // Transmit 1 byte.
+    if (HAL_SPI_Transmit(&m_handler, &value, 1,
+                                              m_kSpiDefaultTimeout) != HAL_OK) {
+        return true;                      
     }
 
-    return (uint8_t)InterfaceNoError;
+    return false;
 }
 
-uint8_t Driver::Spi::sendData(uint8_t reg, uint8_t* data, uint16_t size)
+/**
+ * I2C multiple bytes sending.
+ * @param[i] uint8_t reg - number of device's register.
+ * @param[i] uint8_t* pData - pointer to the byte array to be sent.
+ * @param[i] const uint16_t size - amount of bytes to be sent.
+ * return true - some error was occured during transmission;
+ *        false - no error.
+ **/
+bool Driver::Spi::sendData(uint8_t reg, uint8_t* pData, const uint16_t size)
 {
-    if (HAL_SPI_Transmit(m_handler, data, size, 0x1000) != HAL_OK) {
-        return 0x01;
+    if (!_m_isInit) {
+        return true;
+    }
+    // Transmit 'size' amount of byte.
+    if (HAL_SPI_Transmit(&m_handler, pData, size,
+                                              m_kSpiDefaultTimeout) != HAL_OK) {
+        return true;                      
     }
 
-    return (uint8_t)InterfaceNoError;
+    return false;
 }
 
-uint8_t Driver::Spi::recvByte(uint8_t reg, uint8_t* value)
+/**
+ * I2C byte receiving.
+ * @param[o] uint8_t reg - number of device's register.
+ * @param[i] uint8_t rValue - reference to the variable to be filled by received
+ *                            byte.
+ * return true - some error was occured during receiving;
+ *        false - no error.
+ **/
+bool Driver::Spi::receiveByte(uint8_t reg, uint8_t& rValue)
 {
-    if (HAL_SPI_Receive(m_handler, value, 1, 0x1000) != HAL_OK) {
-        return 0x01;
+    if (!_m_isInit) {
+        return true;
     }
+    // Transfer 1 byte and then receive 1 byte.
+    u8 l_txBuffer[2] = { reg, 0x00 },
+       l_rxBuffer[2] = { 0x00, 0x00 };
+    if (HAL_SPI_TransmitReceive(&m_handler, l_txBuffer, l_rxBuffer, 2, 
+                                              m_kSpiDefaultTimeout) != HAL_OK) {
+        rValue = 0;
+        return true;                      
+    }
+    rValue = l_rxBuffer[1];
 
-    return (uint8_t)InterfaceNoError;
+    return false;
 }   
 
-uint8_t Driver::Spi::recvData(uint8_t reg, uint8_t* data, uint16_t size)
+/**
+ * I2C multiple bytes receiving.
+ * @param[i] uint8_t reg - number of device's register.
+ * @param[o] uint8_t* pData - pointer to the byte array to be filled by received
+ *                            bytes.
+ * @param[i] const uint16_t size - amount of bytes to be received.
+ * return true - some error was occured during transmission;
+ *        false - no error.
+ **/
+bool Driver::Spi::receiveData(uint8_t reg, uint8_t* pData, const uint16_t size)
 {
-    if (HAL_SPI_Receive(m_handler, data, size, 0x1000) != HAL_OK) {
-        return 0x01;
+    if (!_m_isInit) {
+        return true;
     }
+    // Receive data according to 'm_useRegisters' flag.
+    
 
-    return (uint8_t)InterfaceNoError;
+    return false;
 }
