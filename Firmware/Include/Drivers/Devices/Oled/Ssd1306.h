a3952b3ce6dfbea0a9c39e73dc9580075f1f8cf3
Kostyantyn <kostyantyn.komarov@droid-technologies.com>
2019-04-23 11:38:46 +0300

    Rectangle and text drawing methods

--- Firmware/Include/Drivers/Devices/Oled/Ssd1306.h
+++ Firmware/Include/Drivers/Devices/Oled/Ssd1306.h
@@ -1,79 +1,69 @@
 /**
-  *
+  *  @file       Ssd1306.h (header file)
+  *  @version    0.1.0
+  *  @author     utuM (Kostyantyn Komarov)
+  *  @date       19.04.2019 (creation)
+  *  @brief      SSD1306 OLED display controller class.
   **/
 
 #ifndef __DEVICE_SSD1306_H
 #define __DEVICE_SSD1306_H
 
 #include "Device.h"
+#include "Font.h"
+#include "GuiBasics.h"
 #include "I2c.h"
+#include "Typedefs.h"
 
-namespace Driver {
-    const uint8_t s_kSsd1306Address = 0x78;
-    const uint16_t s_kSsd1306ScnBufSize = 512;
-    const uint8_t s_kSsd1306Width = 128;
-    const uint8_t s_kSsd1306Height = 32;
+namespace Driver
+{
+    const u8  s_kSsd1306Address    = 0x78;
+    const u16 s_kSsd1306ScnBufSize = 512;
+    const u8  s_kSsd1306Width      = 128;
+    const u8  s_kSsd1306Height     = 32;
     
+    /**
+      * SSD1306 class.
+      **/
     class Ssd1306 : public Device<I2c>
     {
         public:
-            enum Ssd1306ErrorCode : uint8_t
+            /**
+              * Possible SSD1306 errors.
+              **/
+            enum Ssd1306ErrorCode : u8
             {
-                kSsd1306NoError            = 0x00,
-                kSsd1306InterfaceIsntInit,
+                kSsd1306NoError               = 0x00,
+                kSsd1306ErrInterfaceIsntInit,
+                kSsd1306ErrDeviceIsntInit,
+                kSsd1306ErrInvalidParams,
+                kSsd1306ErrOutOfXBorder
             };
-          
-        private:
-            bool m_isInit;
-            uint8_t m_x;
-            uint8_t m_y;
-            uint8_t m_scrBuffer[s_kSsd1306ScnBufSize];
-            Ssd1306ErrorCode m_error;
 
-            Ssd1306ErrorCode _init(void);             
-            bool _writeCommand(uint8_t command);
-
-        public:
             Ssd1306(void);
 			~Ssd1306(void);
             
-            Ssd1306ErrorCode drawPixel(uint8_t x, uint8_t y, bool isWhite);
+            Ssd1306ErrorCode drawLine(u8 x, u8 y, u8 length, u8 thickness,
+                                                                  flag isWhite);
+            Ssd1306ErrorCode drawRectangle(u8 x0, u8 y0, u8 x1, u8 y1,
+                                           flag isWhite, flag needToFill);
+            Ssd1306ErrorCode drawText(u8 x, u8 y, const str text,
+                                      const Font& rFont,
+                                      const TextAlign alignment, flag isWhite);
             
-            Ssd1306ErrorCode fillScreen(bool isWhite);
+            Ssd1306ErrorCode fillScreen(flag isWhite);
             Ssd1306ErrorCode updateScreen(void);
-            Ssd1306ErrorCode toggle(bool isPowerOn);
+            Ssd1306ErrorCode toggle(flag isPowerOn);
+          
+        private:
+            flag _m_isInit;
+            u8 _m_scrBuffer[s_kSsd1306ScnBufSize];
+            Ssd1306ErrorCode _m_error;
+
+            Ssd1306ErrorCode _init(void);             
+            flag _writeCommand(u8 command); 
+            void _drawPixel(u8 x, u8 y, flag isWhite);
     };
 }
-
-
-////
-////  Enumeration for screen colors
-////
-//typedef enum {
-//	Black = 0x00, // Black color, no pixel
-//	White = 0x01  //Pixel is set. Color depends on LCD
-//} SSD1306_COLOR;
-//
-////
-////  Struct to store transformations
-////
-//typedef struct {
-//	uint16_t CurrentX;
-//	uint16_t CurrentY;
-//	uint8_t Inverted;
-//	uint8_t Initialized;
-//} SSD1306_t;
-//
-////	Definition of the i2c port in main
-//extern I2C_HandleTypeDef SSD1306_I2C_PORT;
-//
-////
-////  Function definitions
-////
-//uint8_t ssd1306_Init(void);
-//void ssd1306_Fill(SSD1306_COLOR color);
-//void ssd1306_UpdateScreen(void);
-//void ssd1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_COLOR color);
-//void ssd1306_SetCursor(uint8_t x, uint8_t y);
 
 #endif // __DEVICE_SSD1306_H
--- Firmware/Source/Drivers/Devices/Oled/Ssd1306.cpp
+++ Firmware/Source/Drivers/Devices/Oled/Ssd1306.cpp
@@ -1,22 +1,16 @@
 /**
-  *
+  *  @file       Ssd1306.cpp (module file)
+  *  @version    0.1.0
+  *  @author     utuM (Kostyantyn Komarov)
+  *  @date       19.04.2019 (creation)
+  *  @brief      SSD1306 OLED display controller class.
   **/
 
 #include "Ssd1306.h"
-
-// Screen object
-/*static SSD1306_t SSD1306;
-
-
-//
-//  Send a byte to the command register
-//
-static void ssd1306_WriteCommand(uint8_t command)
-{
-    
-	HAL_I2C_Mem_Write(&hi2c1,SSD1306_I2C_ADDR, 0x00,1,&command,1,10);
-}*/
-
+  
+/**
+  *
+  **/
 bool Driver::Ssd1306::_writeCommand(uint8_t command)
 {
     if (m_interface.sendByte(0x00, command)) {
@@ -26,70 +20,71 @@ bool Driver::Ssd1306::_writeCommand(uint8_t command)
     return false;
 }
 
+/**
+  *
+  **/
+void Driver::Ssd1306::_drawPixel(u8 x, u8 y, flag isWhite)
+{    
+    if (isWhite) {
+		_m_scrBuffer[x + (y / 8) * s_kSsd1306Width] |= 1 << (y % 8);
+	} 
+	else {
+		_m_scrBuffer[x + (y / 8) * s_kSsd1306Width] &= ~(1 << (y % 8));
+	}
+}    
+
+/**
+  *
+  **/
 Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::_init(void)
 {
     // Wait for the screen to boot.
 	HAL_Delay(100);
 	
-	/* Init LCD */
+	// Init OLED.
 	_writeCommand(0xAE); //display off
     _writeCommand(0xD5); //--set display clock divide ratio/oscillator frequency
 	_writeCommand(0x80); //--set divide ratio
     _writeCommand(0xA8); //--set multiplex ratio(1 to 64)
 	_writeCommand(0x1F); //
-
     _writeCommand(0xD3); //-set display offset
 	_writeCommand(0x00); //-not offset
-
     _writeCommand(0x40); //--set start line address
-
     _writeCommand(0x8D); //--set DC-DC enable
 	_writeCommand(0x14); //
-
     _writeCommand(0x20); //Set Memory Addressing Mode   
 	_writeCommand(0x00); //00,Horizontal Addressing Mode;01,Vertical Addressing Mode;10,Page Addressing Mode (RESET);11,Invalid
-    
     _writeCommand(0xA1); //--set segment re-map 0 to 127
     _writeCommand(0xC8);
-
     _writeCommand(0xDA); //--set com pins hardware configuration
 	_writeCommand(0x02);
-
     _writeCommand(0x81); //--set contrast control register
 	_writeCommand(0x8F);
-
     _writeCommand(0xD9); //--set pre-charge period
 	_writeCommand(0x22); //
-
     _writeCommand(0xDB); //--set vcomh
 	_writeCommand(0x20); //0x20,0.77xVcc
-
     _writeCommand(0xA4); //0xa4,Output follows RAM content;0xa5,Output ignores RAM content
-
     _writeCommand(0xA6); //--set normal display
-
     _writeCommand(0x2E); //--set normal display
-
     _writeCommand(0xAF); //--turn on SSD1306 panel
-
-	/*ssd1306_WriteCommand(0xB0); //Set Page Start Address for Page Addressing Mode,0-7
-	ssd1306_WriteCommand(0xC0); //Set COM Output Scan Direction
-	ssd1306_WriteCommand(0x00); //---set low column address
-	ssd1306_WriteCommand(0x10); //---set high column address*/
 	
+    _m_isInit = true;
 	// Clear screen.
-    fillScreen(true);
+    fillScreen(false);
 	// Flush buffer to screen.
 	updateScreen();
     // Prepare object class before using.
-    memset(m_scrBuffer, 0x00, s_kSsd1306ScnBufSize);	
-	m_isInit = true;
+    memset(_m_scrBuffer, 0x00, s_kSsd1306ScnBufSize);
+    
+    return (_m_error = kSsd1306NoError);
 }
 
-Driver::Ssd1306::Ssd1306(void) : m_isInit(false),
-                                 m_x(0),
-                                 m_y(0),
-                                 m_error(kSsd1306NoError)
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306(void) : _m_isInit(false),
+                                 _m_error(kSsd1306NoError)
 {	
 	// Prepare interface for communication.
     I2cParameters l_parameters = {
@@ -104,139 +99,232 @@ Driver::Ssd1306::Ssd1306(void) : m_isInit(false),
         _init();
     }
     else {
-        m_error = kSsd1306InterfaceIsntInit;
+        _m_error = kSsd1306ErrInterfaceIsntInit;
     }
 }
 
+/**
+  *
+  **/
 Driver::Ssd1306::~Ssd1306(void)
 {
     // Turn the screen into the sleep mode on object destruction.
-    if (m_isInit && m_error == kSsd1306NoError) {
+    if (_m_isInit && _m_error == kSsd1306NoError) {
         toggle(false);
-        m_isInit = false;
+        _m_isInit = false;
     }
 }
 
-Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::drawPixel(uint8_t x,
-                                                             uint8_t y,
-                                                             bool isWhite)
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::drawLine(u8 x,
+                                                            u8 y,
+                                                            u8 length,
+                                                            u8 thickness,
+                                                            flag isWhite)
 {
-    if (!m_isInit && x >= s_kSsd1306Width && y >= s_kSsd1306Height) {
-        return (m_error = kSsd1306InterfaceIsntInit);
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
+    }
+    u8 l_x1 = x + length;
+    u8 l_y1 = y + thickness;
+    if (x >= s_kSsd1306Width || y >= s_kSsd1306Height ||
+        l_x1 >= s_kSsd1306Width || l_y1 >= s_kSsd1306Height) {
+        return (_m_error = kSsd1306ErrInvalidParams);
     }
     
-    if (isWhite) {
-		m_scrBuffer[x + (y / 8) * s_kSsd1306Width] |= 1 << (y % 8);
-	} 
-	else {
-		m_scrBuffer[x + (y / 8) * s_kSsd1306Width] &= ~(1 << (y % 8));
-	}
+    // Draw line according to input parameters.
+    for (u8 i = y; i <= l_y1; i ++) {
+        for (u8 j = x; j <= l_x1; j ++) {
+            _drawPixel(j, i, isWhite);
+        }
+    }
     
-    return (m_error = kSsd1306NoError);
+    return (_m_error = kSsd1306NoError);
 }
 
-Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::fillScreen(bool isWhite)
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306ErrorCode
+                              Driver::Ssd1306::drawRectangle(u8 x0,
+                                                             u8 y0,
+                                                             u8 x1,
+                                                             u8 y1,
+                                                             flag isWhite,
+                                                             flag needToFill)
 {
-    if (!m_isInit) {
-        return (m_error = kSsd1306InterfaceIsntInit);
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
+    }
+    if (x0 >= s_kSsd1306Width || y0 >= s_kSsd1306Height ||
+        x1 >= s_kSsd1306Width || y1 >= s_kSsd1306Height) {
+        return (_m_error = kSsd1306ErrInvalidParams);
+    }
+    
+    // Draw rectangle according to input parameters.
+    for (u8 i = y0; i <= y1; i ++) {
+        for (u8 j = x0; j <= x1; j ++) {
+            if (needToFill) {
+                _drawPixel(i, j, isWhite);
+            }
+            else if ((j == x0 || i == y0) ||
+                     (j == x1 || i == y1)) {
+                _drawPixel(j, i, isWhite);
+            }
+        }
+    }
+    
+    return (_m_error = kSsd1306NoError);
+}
+
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306ErrorCode
+                            Driver::Ssd1306::drawText(u8 x,
+                                                      u8 y,
+                                                      const str text,
+                                                      const Font& rFont,
+                                                      const TextAlign alignment,
+                                                      flag isWhite)
+{
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
     }
   
-	for (uint16_t i = 0; i < s_kSsd1306ScnBufSize; i ++) {
-		m_scrBuffer[i] = (isWhite ? 0xFF : 0x00);
-	}
+    // Check if font size is valid for current screen height.
+    u8 l_symbHeight = rFont.m_height; ///< Maximal symbol height for input font.
+    if (x >= s_kSsd1306Width || y >= s_kSsd1306Height ||
+        (y + l_symbHeight) >= s_kSsd1306Height) {
+        return (_m_error = kSsd1306ErrInvalidParams);
+    }
+    // Count total text width in pixels and update the area that need to be used
+    // for input text.
+    u8 l_areaWidth = 0;               ///< Width of text area.
+    u16 l_textLength = text.length(); ///< Characters amount of the input text.
+    u8 l_symIndex = 0;                ///< Symbol index inside the font.
+    u8 l_currSymWidth = 0;            ///< Current symbol width in pixels.
+    for (u16 i = 0; i < l_textLength; i ++) {
+        if (text[i] != ' ') {
+            l_symIndex = (u8)(text[i]) - (u8)(rFont.m_startSymbol);
+            l_currSymWidth = rFont.m_pDesc[l_symIndex].m_width;
+        }
+        else {
+            l_currSymWidth = rFont.m_spaceWidth;
+        }
+        l_areaWidth += l_currSymWidth;
+        if (i < l_textLength - 1) {
+            l_areaWidth += rFont.m_gapWidth;
+        }
+    }
+    // Limit the right vertical area border position.
+    u8 l_areaX1 = x + l_areaWidth; ///< The text area vertical limit.
+    if (l_areaX1 >= s_kSsd1306Width) {
+        l_areaX1 = s_kSsd1306Width - 1;
+    }
+    // TODO: rectangle's drawing is badly, need to fix this.
+    //drawRectangle(x, y, l_areaX1, y + l_symbHeight, isWhite, yes);
+    // Prepare some additional variables before drawing.
+    u16 l_currSymOffset = 0;          ///< Current symbol offset inside the font
+                                      ///  data buffer.
+    u8 l_x1 = x;                      ///< Next X-ending position.
+    u8 l_y1 = y + l_symbHeight;       ///< Bottom Y position.
+    u16 l_currPosition = 0;           ///< Current byte position in data buffer.
+    u8 l_currBit = 0;                 ///< Current bit offset of data byte.
+    u8 l_widthInBytes = 0;            ///< Amount of bytes in a row for current
+                                      ///  character.
+    // Draw every symbol from the input text.
+    for (u16 n = 0; n < l_textLength; n ++) {
+        // Check if current character is not a 'space'.
+        if (text[n] != ' ') {
+            // Prepare set variables before draw a character.
+            l_symIndex = (u8)(text[n]) - (u8)(rFont.m_startSymbol);
+            l_currSymWidth = rFont.m_pDesc[l_symIndex].m_width;
+            if (n) {
+                x = l_x1 += rFont.m_gapWidth;
+            }
+            l_x1 += l_currSymWidth;
+            
+            l_widthInBytes = (l_currSymWidth / 8) +
+                             ((l_currSymWidth % 8 > 0) ? 1 : 0);
+            l_currSymOffset = rFont.m_pDesc[l_symIndex].m_offset;
+            l_x1 = x + l_currSymWidth;
+            // Prevent false drawing, when end X-position is over the screen
+            // border.
+            if (l_x1 >= s_kSsd1306Width) {
+                return (_m_error = kSsd1306ErrOutOfXBorder);
+            }
+            // Draw current symbol.
+            for (u8 j = y; j < l_y1; j ++) {
+                for (u8 i = x; i < l_x1; i ++) {
+                    l_currPosition = l_currSymOffset +
+                                     (l_widthInBytes * (j - y)) + ((i - x) / 8);
+                    l_currBit = 7 - ((i - x) % 8);
+                    if ((rFont.m_pData[l_currPosition] >> l_currBit) & 0x01) {
+                        _drawPixel(i, j, isWhite);
+                    }
+                }
+            }
+        }
+        else {
+            if (n) {
+                x = l_x1 += rFont.m_gapWidth;
+            }
+            l_x1 += rFont.m_spaceWidth;
+        }
+    }
     
-    return (m_error = kSsd1306NoError);
+    return (_m_error = kSsd1306NoError);
 }
 
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::fillScreen(flag isWhite)
+{
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
+    }
+  
+	for (u16 i = 0; i < s_kSsd1306ScnBufSize; i ++) {
+		_m_scrBuffer[i] = (isWhite ? 0xFF : 0x00);
+	}
+    
+    return (_m_error = kSsd1306NoError);
+}
+
+/**
+  *
+  **/
 Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::updateScreen(void)
 {
-    if (!m_isInit) {
-        return (m_error = kSsd1306InterfaceIsntInit);
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
     }
   
-    for (uint8_t i = 0; i < 8; i++) {
+    for (u8 i = 0; i < 8; i++) {
 		_writeCommand(0xB0 + i);
 		_writeCommand(0x00);
 		_writeCommand(0x10);
 
-		m_interface.sendData(0x40, &m_scrBuffer[s_kSsd1306Width * i], s_kSsd1306Width);
+		m_interface.sendData(0x40, &_m_scrBuffer[s_kSsd1306Width * i], s_kSsd1306Width);
 	}
     
-    return (m_error = kSsd1306NoError);
+    return (_m_error = kSsd1306NoError);
 }
 
-Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::toggle(bool isPowerOn)
+/**
+  *
+  **/
+Driver::Ssd1306::Ssd1306ErrorCode Driver::Ssd1306::toggle(flag isPowerOn)
 {
-
+    if (!_m_isInit) {
+        return (_m_error = kSsd1306ErrDeviceIsntInit);
+    }
+  
+    // TODO: implement properly power toggle process.
+    
+    return (_m_error = kSsd1306NoError);
 }
-
-//
-////
-////  Fill the whole screen with the given color
-////
-//void ssd1306_Fill(SSD1306_COLOR color) 
-//{
-//	/* Set memory */
-//	uint32_t i;
-//
-//	for(i = 0; i < sizeof(SSD1306_Buffer); i++)
-//	{
-//		SSD1306_Buffer[i] = (color == Black) ? 0x00 : 0xFF;
-//	}
-//}
-//
-////
-////  Write the screenbuffer with changed to the screen
-////
-//void ssd1306_UpdateScreen(void) 
-//{
-//	uint8_t i;
-//	
-//	for (i = 0; i < 8; i++) {
-//		ssd1306_WriteCommand(0xB0 + i);
-//		ssd1306_WriteCommand(0x00);
-//		ssd1306_WriteCommand(0x10);
-//
-//		HAL_I2C_Mem_Write(&hi2c1,SSD1306_I2C_ADDR,0x40,1,&SSD1306_Buffer[SSD1306_WIDTH * i], SSD1306_WIDTH, 100);
-//	}
-//}
-//
-/////
-////	Draw one pixel in the screenbuffer
-////	X => X Coordinate
-////	Y => Y Coordinate
-////	color => Pixel color
-////
-//void ssd1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_COLOR color)
-//{
-//	if (x >= SSD1306_WIDTH || y >= SSD1306_HEIGHT) 
-//	{
-//		// Don't write outside the buffer
-//		return;
-//	}
-//	
-//	// Check if pixel should be inverted
-//	if (SSD1306.Inverted) 
-//	{
-//		color = (SSD1306_COLOR)!color;
-//	}
-//	
-//	// Draw in the right color
-//	if (color == White)
-//	{
-//		SSD1306_Buffer[x + (y / 8) * SSD1306_WIDTH] |= 1 << (y % 8);
-//	} 
-//	else 
-//	{
-//		SSD1306_Buffer[x + (y / 8) * SSD1306_WIDTH] &= ~(1 << (y % 8));
-//	}
-//}
-//
-////
-////	Position the cursor
-////
-//void ssd1306_SetCursor(uint8_t x, uint8_t y) 
-//{
-//	SSD1306.CurrentX = x;
-//	SSD1306.CurrentY = y;
-//}
