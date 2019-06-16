7eb5bd84d0117159abf6bf052905a4366f6cfc98
Unknown <utu.kos@gmail.com>
2019-06-16 15:51:38 +0300

    LL common files and other stuff init commit
    'Typedefs' implementation

---
+++ Firmware/Include/LL_Drivers/Common/Typedefs.h
@@ -0,0 +1,32 @@
+#ifndef __CORE_TYPEDEFS_H__
+#define __CORE_TYPEDEFS_H__
+
+#include <stdbool.h>
+
+#define     unk          void          
+#define     flg          bool
+#define     s16          short          
+#define     s32           int           
+#define     s64        long long        
+#define     u8        unsigned char      
+#define     u16      unsigned short     
+#define     u32       unsigned int      
+#define     u64    unsigned long long   
+#define     sym          char          
+#define     enm      typedef enum      
+#define     stc     typedef struct     
+
+#define     punk          void*     
+#define     pflg          bool*
+#define     ps16          short*         
+#define     ps32           int*          
+#define     ps64        long long*       
+#define     pu8       unsigned char*     
+#define     pu16     unsigned short*    
+#define     pu32      unsigned int*     
+#define     pu64   unsigned long long*  
+#define     psym          char*         
+#define     penm      typedef enum*     
+#define     pstc     typedef struct*    
+
+#endif // __CORE_TYPEDEFS_H__
